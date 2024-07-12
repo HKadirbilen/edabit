d60e7ca9dcfbd7ac5bf8e0188eec72ee6a9f2d30
Hüseyin Kadir Bilen <88401660+HKadirbilen@users.noreply.github.com>
2024-07-12 16:37:52 +0300

    first commit

--- 01-very-easy/001-return-the-sum-of-two-numbers.py
+++ 01-very-easy/001-return-the-sum-of-two-numbers.py
@@ -14,8 +14,6 @@ Don't forget to return the result.
 If you get stuck on a challenge, find help in the Resources tab.
 If you're really stuck, unlock solutions in the Solutions tab.
 
-
-zafer
 """
 
 
